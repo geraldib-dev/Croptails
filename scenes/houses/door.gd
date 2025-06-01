6156d563a39d3cc145d4c4b0f6b95566260677c8
Gerald Ibra <gi@horologists.com>
2025-06-01 02:10:21 +0200

    Added different houses and tilemaps and layers

---
+++ scenes/houses/door.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var interactactable_component: InteractableComponent = $InteractactableComponent
+
+func _ready() -> void:
+	interactactable_component.interactable_activated.connect(on_interactable_activated)
+	interactactable_component.interactable_deactivated.connect(on_interactable_deactivated)
+	collision_layer = 1
+	
+func on_interactable_activated() -> void:
+	animated_sprite_2d.play("open_door")
+	collision_layer = 2
+	print("activated")
+
+func on_interactable_deactivated() -> void: 
+	animated_sprite_2d.play("close_door")
+	collision_layer = 1
+	print("deactivated")
